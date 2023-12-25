d54862fe2b701fe7e61fb33ace4b4860fa187d3e
Donnie Barnes <djb@donniebarnes.com>
2023-12-24 20:39:53 -0500

    First commit

---
+++ README.md
@@ -0,0 +1,26 @@
+# gate-controller
+
+A Particle Photon2 project named gate-controller-photon2
+
+This code is based on my gate-controller code which originally used a Particle Photon (first generation) plugged into an NCD2Relay board. The Photon2, however, uses the Feather form factor instead of the form factor of the previous generation Photon and as such won't plug into the NCD2Relay board. It would be possible to adapt it, but it's cheaper and easier to just plug it into the Adafruit non latching relay board for Feather. That means simplified code since it is simply pulling a pin high and low (D2) to control the relay instead of needing to talk to an I/O extender, which is what the NCD2Relay board uses.
+
+Publishes {controller name}_1 on/off when the relay state is changed.
+
+Relay state can be changed by the cloud by any device publishing {controller name}_relay_1 with “1on” or “1off”.
+
+Relay state can be gathered from all gates by publishing "getstate" to particle cloud.
+
+Future commands can be skipped by publishing "skipcommand X" where X is 0-9. For example, if you publish "skipcommand 2" then every gate will skip the next two commands sent to it by particle cloud.
+
+Relay state can also be changed by the Particle app under Functions. (Warning: this does not trigger the firmware to publish any status to particle cloud. It also does NOT count as a "command" as far as skips are concerned.)
+
+
+## Compiling your project
+
+When you're ready to compile your project, make sure you have the correct Particle device target selected and run `particle compile <platform>` in the CLI or click the Compile button in the Desktop IDE. The following files in your project folder will be sent to the compile service:
+
+- Everything in the `/src` folder, including your `.ino` application file
+- The `project.properties` file for your project
+- Any libraries stored under `lib/<libraryname>/src`
+
+You then just need to run 'particle flash <device_name>' to flast your compiled code to your Particle Photon2
---
+++ src/gate-controller-photon2.ino
@@ -0,0 +1,141 @@
+/*
+ * Project gate-controller-photon2
+ * Description: control the gate!
+ * Author: Donnie Barnes <djb@donniebarnes.com>
+ * Date: December 24, 2023
+ */
+
+#include <stdlib.h>
+
+// NCD2Relay relayController;
+
+SYSTEM_MODE(AUTOMATIC);
+
+int RELAY = D2;
+bool relay_state = 0;
+
+int triggerRelay(String command);
+
+bool tripped[6];
+
+int debugTrips[6];
+
+int minTrips = 5;
+int skipCounter = 0;
+
+String eventName;
+
+char dev_name[32] = "";
+char pubString[40] = "";
+char restarted_msg[11] = "_restarted";
+bool publishName = false;
+
+void handler(const char *topic, const char *data){
+    strncpy(dev_name, data, sizeof(dev_name)-1);
+    publishName = true;
+}
+
+void pubState(const char *topic, const char *data){
+	char pub[40] = "";
+	strcat(pub, pubString);
+	strcat(pub, "_1");
+        char skips[sizeof(int)*8+1] = "";
+        itoa(skipCounter, skips, 10);
+        char stats[sizeof(int)*8+5] = "";
+	if (relay_state) {
+            strcat(stats, "ON ");
+            strcat(stats, skips);
+            Particle.publish(pub, stats, PRIVATE);
+        }
+	else {
+            strcat(stats, "OFF ");
+            strcat(stats, skips);
+            Particle.publish(pub, stats, PRIVATE);
+        }
+
+}
+
+void skipCommand(const char *topic, const char *data){
+        skipCounter = atoi(data);
+        pubState(NULL, NULL);
+}
+
+/* This function is called once at start up ----------------------------------*/
+void setup()
+{
+
+        pinMode(RELAY, OUTPUT);
+        digitalWrite(RELAY, LOW); // close gate by default at power up
+        relay_state = 0;
+	Particle.function("controlRelay", triggerRelay);
+	Particle.subscribe("particle/device/name", handler);
+    Particle.publish("particle/device/name");
+    // allow for up to 3 sec and maximum time to attend to cloud tasks
+    for (uint32_t ms = millis(); millis() - ms < 3000 && !publishName; Particle.process());
+	Serial.begin(115200);
+	Serial.println(dev_name);
+	strcat(pubString, dev_name);
+	strcat(dev_name, "_relay_1");
+	Particle.subscribe(dev_name, triggerRelayDos, MY_DEVICES);
+
+	// listen for monitors to ask for state and if they do, report it (this report is the same
+	// as what gets reported when the gate state is changed)
+	Particle.subscribe("getstate", pubState, MY_DEVICES);
+
+        // listen for "skipcommand" if then increment the skipCounter if given
+	Particle.subscribe("skipcommand", skipCommand, MY_DEVICES);
+
+	// publish state on boot up, too
+	pubState(NULL, NULL);
+
+
+}
+
+/* This function loops forever --------------------------------------------*/
+void loop()
+{
+}
+
+int triggerRelayDos(const char *event, const char *data){
+    if(skipCounter==0){
+        Serial.println(data);
+        triggerRelay(data);
+    }
+    else {
+        skipCounter--;
+        pubState(NULL, NULL);
+    }
+    return 1;
+}
+
+int triggerRelay(String command){
+	//Relay Specific Command
+	eventName = pubString;
+	eventName+="_";
+	int relayNumber = command.substring(0,1).toInt();
+	Serial.print("relayNumber: ");
+	Serial.println(relayNumber);
+	String relayCommand = command.substring(1);
+	Serial.print("relayCommand:");
+	Serial.print(relayCommand);
+	Serial.println(".");
+	if(relayCommand.equalsIgnoreCase("on")){
+		Serial.println("Turning on relay");
+                relay_state = 1;
+                digitalWrite(RELAY, HIGH);
+		eventName+=(relayNumber);
+		Particle.publish(eventName, "ON", PRIVATE);
+		Serial.println("returning");
+		return 1;
+	}
+	if(relayCommand.equalsIgnoreCase("off")){
+                relay_state = 0;
+                digitalWrite(RELAY, LOW);
+		eventName+=(relayNumber);
+		Particle.publish(eventName, "OFF", PRIVATE);
+
+		return 1;
+	}
+	return 0;
+}
+
